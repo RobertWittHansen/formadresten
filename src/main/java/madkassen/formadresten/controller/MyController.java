c62a81d54d79e87c8a830c1f2e2ffc1cc04e568a
Robert Witt Hansen <along_the_black_river@hotmail.com>
2021-03-09 16:34:51 +0100

    Super1 super2

--- src/main/java/madkassen/formadresten/controller/MyController.java
+++ src/main/java/madkassen/formadresten/controller/MyController.java
@@ -3,6 +3,9 @@ package madkassen.formadresten.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 public class MyController
@@ -13,5 +16,34 @@ public class MyController
         return "index";
     }
 
+    @GetMapping("/forbrugeren")
+    public String forbrugeren()
+    {
+        return "forbrugeren";
+    }
 
+    @GetMapping("/virksomhed")
+    public String virksomhed()
+    {
+        return "virksomhed";
+    }
+
+    @GetMapping("/organnsation")
+    public String organisation()
+    {
+        return "organnsation";
+    }
+
+    @PostMapping("/")
+    public String tavleopslag()
+    {
+
+        return  "redirect: /opslagstavlen";
+    }
+
+    @GetMapping("/opslagstavlen")
+    public String opslagstavlen()
+    {
+        return "opslagstavlen";
+    }
 }
--- src/main/java/madkassen/formadresten/model/Madkassen.java
+++ src/main/java/madkassen/formadresten/model/Madkassen.java
@@ -1,4 +1,36 @@
 package madkassen.formadresten.model;
 
-public class Madkassen {
+public class Madkassen
+{
+    private String firstName;
+    private String lastName;
+
+    private String opslagstavlen;
+
+    private String forbrugeren;
+    private String virksomhed;
+    private String organisation;
+
+
+    // Constructer.
+    public Madkassen( String firstName, String lastName, String opslagstavlen, String forbrugeren, String virksomhed, String organisation)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.opslagstavlen = opslagstavlen;
+        this.forbrugeren = forbrugeren;
+        this.virksomhed = virksomhed;
+        this.organisation = organisation;
+    }
+
+    // Getter.
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getOpslagstavlen() { return opslagstavlen; }
+    public String getForbrugeren() { return forbrugeren; }
+    public String getVirksomhed() { return virksomhed; }
+    public String getOrganisation() { return organisation; }
+
+
+
 }
