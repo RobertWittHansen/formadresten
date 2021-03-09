25ac9921babd75e3778d179b30eeab1a04ff6e98
mathslothkea <math68n7@stud.kea.dk>
2021-03-09 11:59:08 +0100

    Udkast

--- src/main/java/madkassen/formadresten/controller/MyController.java
+++ src/main/java/madkassen/formadresten/controller/MyController.java
@@ -7,10 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController
 {
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index()
     {
         return "index";
+    }
+
+    @GetMapping("/forbrugeren")
+    public String forbrugeren()
+    {
+        return "forbrugeren";
+    }
+
+    @GetMapping("/organisation")
+    public String organisation()
+    {
+        return "organisation";
+    }
+
+    @GetMapping("/virksomhed")
+    public String virksomhed()
+    {
+        return "virksomhed";
     }
 
 
